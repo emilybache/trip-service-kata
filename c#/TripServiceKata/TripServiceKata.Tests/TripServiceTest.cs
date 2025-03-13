a5300de722b22c9ae41f7127d4aa8b31e84a0ba7
emily <emily@bacheconsulting.com>
2025-03-13 10:50:43 +0100

    update .net version and add dummy test

--- c#/TripServiceKata/TripServiceKata.Tests/TripServiceTest.cs
+++ c#/TripServiceKata/TripServiceKata.Tests/TripServiceTest.cs
@@ -1,6 +1,13 @@
-﻿namespace TripServiceKata.Tests
+﻿using Xunit;
+
+namespace TripServiceKata.Tests
 {
     public class TripServiceTest
     {
+        [Fact]
+        public void testSomething()
+        {
+            Assert.Equal(1, 2);
+        }
     }
 }
